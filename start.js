465de5b8f355be0cf3afee3bfadb91699efd5a12
Kohei TAKATA <kt.koheitakata@gmail.com>
2016-02-15 22:56:54 +0900

    authentication

--- start.js
+++ start.js
@@ -4,4 +4,18 @@ const server =new DeepstreamServer();
 server.set('host', 'localhost');
 server.set('port', 6020);
 
+server.set('permissionHandler', {
+   isValidUser: function(connectionData, authData, callback) {
+       if( authData.user === 'kohei' && authData.password === 'password' ) {
+           callback( null, authData.user );
+       } else {
+           callback( 'invalid credentials' );
+       }
+    },
+    canPerformAction: function(username, message, callback) {
+        callback(null, true);
+    },
+    onClientDisconnect: function(username) {}
+});
+
 server.start();
